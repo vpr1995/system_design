2619ff3f1474b207dd54c68b827f656b56f18e3b
Prudhvi Reddy Vemireddy <vprprudhvi@gmail.com>
2024-02-03 23:25:48 -0600

    tsrtsdrtsdt

--- services/url_shortner/test/url_shortner.test.ts
+++ services/url_shortner/test/url_shortner.test.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Template, Match } from 'aws-cdk-lib/assertions';
 import * as UrlShortner from '../lib/url_shortner-stack';
 
-test('SQS Queue and SNS Topic Created', () => {
+test('SQS Queue and SNS Topic   Created', () => {
   const app = new cdk.App();
   // WHEN
   const stack = new UrlShortner.UrlShortnerStack(app, 'MyTes  tStac k');
